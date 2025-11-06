241c632e83da97f1cd86921ebada6b8200d7251c
GIOVANNE ISAAC MARQUES <25170154@educ123.sp.senai.br>
2025-11-06 09:44:55 -0300

    login div gerais

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -1,3 +1,10 @@
 export default function Login () {
-    return <></>
+    return <>
+        <div className="d-flex vh-100">
+            <div className="col-6 fundoAzul1"></div>
+            <div className="col-6 shadow-lg border d-flex justify-content-center align-itens-center">
+
+            </div>
+        </div>
+    </>
 }
