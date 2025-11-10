87e1b4945e5b71cf644f7288338814e17e084683
GIOVANNE ISAAC MARQUES <25170154@educ123.sp.senai.br>
2025-11-10 08:48:00 -0300

    cssHome

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -3,33 +3,36 @@ import styles from "./page.module.css";
 import './main.css';
 
 export default function Home() {
-  return (<>
+	return (<>
+		<div className="subtitles fontetop size-fonte">
+			{/* Titulo */}
+			<div className="border-gm-color">
+				<h1>
+					Sistema inovador para gestão de treinamento
+				</h1>
+			</div>
 
-    <div className="subtitles fontetop size-fonte">
-      <div className="border-gm-color">
-        <h1>
-          Sistema inovador para gestão de treinamento
-        </h1>
-      </div>
-      <div className="d-flex justify-content-center align-items-center">
-        <h1>
-          {`Projetado de acordo com a`}<br />{`necessidade do `}
-          <span className="operador text-white px-2">operador</span>
-        </h1>
-      </div>
-      <div className="tamanho">
-        <p>Centralize a gestão de competências e treinamentos da sua equipe.
-          Solicite novos usuários, acompanhe o progresso e acesse relatórios e dashboards para decisões mais estratégicas.</p>
-      </div>
-      <div>
-        <Image
-          src="/img/fabrica3.png"
-          width={1920}
-          height={628}
-          alt="Imagem para banner 1" />
-      </div>
-    </div>
+			{/* Descrição */}
+			<div className="d-flex justify-content-center align-items-center">
+				<h1>
+					{`Projetado de acordo com a`}<br />{`necessidade do `}
+					<span className="operador text-white px-2">operador</span>
+				</h1>
+			</div>
+			<div className="">
+				<p>Centralize a gestão de competências e treinamentos da sua equipe.
+					Solicite novos usuários, acompanhe o progresso e acesse relatórios e dashboards para decisões mais estratégicas.</p>
+			</div>
 
-  </>
-  );
+			{/* Banner */}
+			<div>
+				<Image
+					src="/img/fabrica3.png"
+					width={1920}
+					height={628}
+					alt="Imagem para banner 1" />
+			</div>
+		</div>
+	</>
+	);
 }
